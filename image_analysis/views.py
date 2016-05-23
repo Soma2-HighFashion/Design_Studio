2cdc48ce75c031510abeed7d0aa2f0813faf2425
DongjunLee <ehdwns2045@gmail.com>
2016-05-23 18:27:16 +0900

    Add jQuery UI - ProgressBar, Connect image_analysis

--- image_analysis/views.py
+++ image_analysis/views.py
@@ -2,6 +2,7 @@
 from __future__ import division, print_function, absolute_import
 
 import os
+import time
 from subprocess import Popen, PIPE, STDOUT
 
 import numpy as np
@@ -15,38 +16,9 @@ from django.http import JsonResponse
 
 from image_analysis.data import generate_patches, img2numpy_arr
 
-# Create your views here.
-def classify_gender(request):
-	input_image_uid = str(request.GET['input'])
-	design_path = "/home/dj/HighFashionProject/design_studio/static_files/designed/"
+def construct_network():
+	tl.init_graph(num_cores=8, gpu_memory_fraction=0.4)
 
-	X = generate_patches(img2numpy_arr(design_path + input_image_uid))
-
-	model = construct_model(2)
-	image_analysis_path = "/home/dj/HighFashionProject/design_studio/image_analysis/"
-	model.load(image_analysis_path + 'vgg_gender_model-80000')
-	pred_y = model.predict(X)
-
-	return JsonResponse(
-		{'results' : pred_y}
-	)
-
-def classify_category(request):
-	input_image_uid = str(request.GET['input'])
-	design_path = "/home/dj/HighFashionProject/design_studio/static_files/designed/"
-
-	X = generate_patches(img2numpy_arr(design_path + input_image_uid))
-
-	model = construct_model(6)
-	image_analysis_path = "/home/dj/HighFashionProject/design_studio/image_analysis/"
-	model.load(image_analysis_path + 'vgg_category_model-130000')
-	pred_y = model.predict(X)
-
-	return JsonResponse(
-		{'results' : pred_y}
-	)
-
-def construct_model(num_classes):
 	# Building 'VGG Network'
 	network = input_data(shape=[None, 84, 84, 3])
 
@@ -77,6 +49,7 @@ def construct_model(num_classes):
 	network = dropout(network, 0.5)
 	network = fully_connected(network, 4096, activation='relu')
 	network = dropout(network, 0.5)
+
 	network = fully_connected(network, num_classes, activation='softmax')
 
 	network = regression(network, optimizer='rmsprop',
@@ -85,5 +58,87 @@ def construct_model(num_classes):
 	# Model
 	model = tl.DNN(network)
 	return model
+
+#image_analysis_path = "/home/dj/HighFashionProject/design_studio/image_analysis/"	
+#gender_model = construct_model(2)
+#gender_model.load(image_analysis_path + 'vgg_gender_model-80000')
+#print("Complete - load gender model")
+
+#category_model = construct_model(6)
+#category_model.load(image_analysis_path + 'vgg_category_model-130000')
+#print("Complete - load category model")
+	
+
+tl.init_graph(num_cores=8, gpu_memory_fraction=0.4)
+
+# Building 'VGG Network'
+network = input_data(shape=[None, 84, 84, 3])
+
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 128, 3, activation='relu')
+network = conv_2d(network, 128, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 256, 3, activation='relu')
+network = conv_2d(network, 256, 3, activation='relu')
+network = conv_2d(network, 256, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = fully_connected(network, 4096, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 4096, activation='relu')
+vgg_network = dropout(network, 0.5)
+
+image_analysis_path = "/home/dj/HighFashionProject/design_studio/image_analysis/"	
+
+# Create your views here.
+def classify_gender(request):
+	input_image_uid = str(request.GET['input'])
+	design_path = "/home/dj/HighFashionProject/design_studio/static_files/designed/"
+	X = generate_patches(img2numpy_arr(design_path + input_image_uid))
+
+	num_classes = 2
+	network_output = fully_connected(vgg_network, num_classes, activation='softmax')
+	network_output = regression(network_output, optimizer='rmsprop',
+						 loss='categorical_crossentropy',
+						 learning_rate=0.00001)
+	# Model
+	gender_model = tl.DNN(network_output)
+	gender_model.load(image_analysis_path + 'vgg_gender_model-80000')
+	pred_y = gender_model.predict(X)
+	return JsonResponse(
+		{'results' : pred_y}
+	)
+
+def classify_category(request):
+	input_image_uid = str(request.GET['input'])
+	design_path = "/home/dj/HighFashionProject/design_studio/static_files/designed/"
+	X = generate_patches(img2numpy_arr(design_path + input_image_uid))
+
+	num_classes = 6
+	network_output = fully_connected(network, num_classes, activation='softmax')
+	network_output = regression(network_output, optimizer='rmsprop',
+						 loss='categorical_crossentropy',
+						 learning_rate=0.00001)
+	# Model
+	category_model = tl.DNN(network_output)
+	category_model.load(image_analysis_path + 'vgg_category_model-130000')
+	pred_y = category_model.predict(X)
+	return JsonResponse(
+		{'results' : pred_y}
+	)
 
 
--- static/js/design/step.js
+++ static/js/design/step.js
@@ -83,51 +83,89 @@ StepTwo.prototype.next = function() {
 	var circle_image = $("#step3_circle_image");
 	circle_image.attr('src', designedPath + selectedText);
 
+	var patchCount = 25
 	classifyGenderImage(selectedText, function(response) {
-		alert("Gender : " + response.results);
+		var female = 0; var male = 0
+		response.results.forEach(function(item, index) {
+			female += item[0];
+			male += item[1];
+		});
+		
+		female = Math.round(female*4);
+		male = Math.round(male*4);
+
+		var femaleProgressBar = $("#classify_gender_female");
+		femaleProgressBar.progressbar({
+			value: female
+		});
+		var femaleProgressBarValue = femaleProgressBar.find(".ui-progressbar-value");
+		femaleProgressBarValue.css({
+			"background": '#C08080'
+		});
+		
+		var maleProgressBar = $("#classify_gender_male");
+		maleProgressBar.progressbar({
+			value: male
+		});
+		var maleProgressBarValue = maleProgressBar.find(".ui-progressbar-value");
+		maleProgressBarValue.css({
+			"background": '#438C56'
+		});
 	});
 
-	classifyCategoryImage(selectedText, function(response) {
-		alert("Category : " + response.results);
-	});
+//	classifyCategoryImage(selectedText, function(response) {
+//		category = [0, 0, 0, 0, 0, 0];
+//		response.results.forEach(function(item, index) {
+//			for(var i=0; i<6; i++) {
+//				category[i] += item[i];
+//			}
+//		});
+//
+//		category.forEach(function(item, index) {
+//			category[index] = Math.round(item*4);
+//		});
+//
+//		console.log(category);
+//
+//		var myChart = echarts.init(document.getElementById('echart_pie2'), theme);
+//		myChart.setOption({
+//		  tooltip: {
+//			trigger: 'item',
+//			formatter: "{a} <br/>{b} : {c} ({d}%)"
+//		  },
+//		  calculable: true,
+//		  series: [{
+//			name: 'Area Mode',
+//			type: 'pie',
+//			radius: [25, 90],
+//			center: ['50%', 170],
+//			roseType: 'area',
+//			x: '50%',
+//			max: 100,
+//			sort: 'ascending',
+//			data: [{
+//			  value: category[0],
+//			  name: 'Street'
+//			}, {
+//			  value: category[1],
+//			  name: 'Casual'
+//			}, {
+//			  value: category[2],
+//			  name: 'Sexy'
+//			}, {
+//			  value: category[3],
+//			  name: 'Unique'
+//			}, {
+//			  value: category[4],
+//			  name: 'Work wear'
+//			}, {
+//			  value: category[5],
+//			  name: 'Classic'
+//			}]
+//		  }]
+//		});
+//	});
 
-	var myChart = echarts.init(document.getElementById('echart_pie2'), theme);
-    myChart.setOption({
-      tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-      calculable: true,
-      series: [{
-        name: 'Area Mode',
-        type: 'pie',
-        radius: [25, 90],
-        center: ['50%', 170],
-        roseType: 'area',
-        x: '50%',
-        max: 40,
-        sort: 'ascending',
-        data: [{
-          value: 10,
-          name: 'Street'
-        }, {
-          value: 5,
-          name: 'Casual'
-        }, {
-          value: 15,
-          name: 'Sexy'
-        }, {
-          value: 25,
-          name: 'Unique'
-        }, {
-          value: 20,
-          name: 'Work wear'
-        }, {
-          value: 35,
-          name: 'Classic'
-        }]
-      }]
-    });
 }
 
 function StepThree(nextStep) {
